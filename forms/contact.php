ea1c1d9d83ec251a90d8826df94ee03a2eaee088
Rabin Kunwar <167570865+rabin-kunwar@users.noreply.github.com>
2025-09-07 11:42:32 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,36 +1,3 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name    = strip_tags(trim($_POST["name"]));
-    $email   = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $subject = strip_tags(trim($_POST["subject"]));
-    $message = trim($_POST["message"]);
 
-    if (empty($name) || empty($subject) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        http_response_code(400);
-        echo "Please fill all fields correctly.";
-        exit;
-    }
 
-    $to = "rabinkunwar055@gmail.com"; // Your email address
-
-    $email_content = "New message from portfolio contact form:\n\n";
-    $email_content .= "Name: $name\n";
-    $email_content .= "Email: $email\n";
-    $email_content .= "Subject: $subject\n";
-    $email_content .= "Message:\n$message\n";
-
-    $email_headers = "From: $name <$email>";
-
-    if (mail($to, $subject, $email_content, $email_headers)) {
-        http_response_code(200);
-        echo "Your message has been sent.";
-    } else {
-        http_response_code(500);
-        echo "Message could not be sent. Try again later.";
-    }
-} else {
-    http_response_code(403);
-    echo "There was a problem with your request.";
-}
-?>
 
